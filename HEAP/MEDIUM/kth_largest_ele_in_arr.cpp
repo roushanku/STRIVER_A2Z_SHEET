e210424a950b6d06cd0fb3589787f4c0703b4452
roushanku <roushankuiitbhilai.ac.in>
2024-02-02 12:03:57 +0530

    using priority_queue

---
+++ HEAP/MEDIUM/kth_largest_ele_in_arr.cpp
@@ -0,0 +1,37 @@
+// J.S.R -- *roush*
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1000000007;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds; //find_by_order //order_of_key
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue <int , vector <int> , greater<int>> pq;
+
+        for(int i = 0 ; i < nums.size() ; i++) {
+            pq.push(nums[i]);
+            if(pq.size() > k) pq.pop();
+        }
+
+        int ans = pq.top();
+
+        return ans;
+    }
+};
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+//patience Bro , thoda sa logic soch k try ur own test case
+//hadbad m kaafi gadbad kie ho islie aaram s -> You will do best for Youself
+//First, solve the problem. Then, write the code.
+int t;
+cin>>t;
+while(t--){
+           
+}
+return 0;
+}
